56683cc5166a9f4827847d066c15f12d62d86570
Jonathan Guiang <jkguiang@gmail.com>
2021-07-02 16:30:47 -0700

    removed uncertainties (not meaningful), added muon POG medium ID for kicks

--- VBSHWWCORE/tools/lepScaleFactors.h
+++ VBSHWWCORE/tools/lepScaleFactors.h
@@ -4,18 +4,15 @@
 #include "Base.h"
 
 namespace ttH {
-    float getElecPOGLooseSFErr(float eta, float pt, int year);
-    float getMuonTightSFErr(float eta, float pt, int year);
-    float getElecLooseSF(float eta, float pt, int year);
-    float getMuonLooseSFErr(float eta, float pt, int year);
+    float getElecTightSF(float eta, float pt, int year);
     float getMuonTightSF(float eta, float pt, int year);
-    float getElecTightSFErr(float eta, float pt, int year);
+    float getElecLooseSF(float eta, float pt, int year);
     float getMuonLooseSF(float eta, float pt, int year);
     float getElecPOGLooseSF(float eta, float pt, int year);
-    float getElecTightSF(float eta, float pt, int year);
-    float getElecLooseSFErr(float eta, float pt, int year);
-    float getElecRecoEffSFErr(float eta, float pt, int year);
     float getElecRecoEffSF(float eta, float pt, int year);
+    float getMuonPOGMediumSF2016GH(float eta, float pt, int year);
+    float getMuonPOGMediumSF2016BCDEF(float eta, float pt, int year);
+    float getMuonPOGMediumSF(float eta, float pt, int year);
 }
 
 #endif
