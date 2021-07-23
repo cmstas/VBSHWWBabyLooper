a8d1b7bedb5db3bff5642d4ef398d940d9cdc9d6
Philip Chang <sgnoohc@gmail.com>
2021-07-22 17:12:32 -0700

    update

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 rm -f run.log
 
 # Run the ReReco 2016
-time sh scripts/run_v4.sh -s createMini -t v2.3_SS >> run.log 2>&1
+time sh scripts/run_v4.sh -s createMini -t v2.3_SS | tee -a run.log 2>&1
 # Run the ReReco 2017/2018
-time sh scripts/run_UL.sh -s createMini -t v2.3_SS -T v2.3_SS >> run.log 2>&1
+time sh scripts/run_UL.sh -s createMini -t v2.3_SS -T v2.3_SS | tee -a run.log 2>&1
