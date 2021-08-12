0b6a4ca624b3b1b7504c0e83d97ee9dd6719dc36
Jonathan Guiang <jkguiang@gmail.com>
2021-08-12 14:25:28 -0700

    initial commit

---
+++ VBSHWWCORE/tools/pileUpReweight.h
@@ -0,0 +1,12 @@
+#ifndef PILEUPREWEIGHT_H
+#define PILEUPREWEIGHT_H
+
+float pileUpReweight(int n_vtx, int year);
+float pileUpReweightUp(int n_vtx, int year);
+float pileUpReweightDown(int n_vtx, int year);
+
+float pileUpReweightUL(int n_vtx, int year);
+float pileUpReweightUpUL(int n_vtx, int year);
+float pileUpReweightDownUL(int n_vtx, int year);
+
+#endif
