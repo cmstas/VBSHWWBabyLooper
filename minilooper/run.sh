d9faa9d8a3ec4b17de2ced3d934f17d741a5b0ca
Jonathan Guiang <jkguiang@gmail.com>
2021-08-31 10:49:42 -0700

    modified to run jecUp/Dn hists as well before running alpa_exp study script

--- minilooper/run.sh
+++ minilooper/run.sh
@@ -1,12 +1,23 @@
-RUNTAG=Nominal
+RUNTAG=Final
 SKIMVERSION=v2.6_SS
-BABYVERSION=v10
-USERNAME=phchang
+USERNAME=jguiang
 YEAR=Run2
-sh scripts/run.sh ${RUNTAG} ${SKIMVERSION} ${BABYVERSION} ${YEAR} ${USERNAME}
-rm -rf /nfs-7/userdata/${USERNAME}/VBSHWWResult/${SKIMVERSION}/${BABYVERSION}/${YEAR}/${RUNTAG}
-sh scripts/publish_result.sh ${RUNTAG} ${SKIMVERSION} ${BABYVERSION} ${YEAR}
-python scripts/alpha_exp_study.py ${RUNTAG} ${SKIMVERSION} ${BABYVERSION} ${YEAR} ${USERNAME}
-sh scripts/make_figures.sh ${RUNTAG} ${SKIMVERSION} ${BABYVERSION} ${YEAR} ${USERNAME}
-ln -fs /nfs-7/userdata/${USERNAME}/VBSHWWResult/${SKIMVERSION}/${BABYVERSION}/${YEAR}/${RUNTAG}/plots plots
-ln -fs /nfs-7/userdata/${USERNAME}/VBSHWWResult/${SKIMVERSION}/${BABYVERSION}/${YEAR}/${RUNTAG}/topbkgstudy topbkgstudy
+
+BABYVERSIONS="v2_jecUp
+v2_jecDn
+v2"
+
+for BABYVERSION in ${BABYVERSIONS}; do
+    # Run minilooper
+    sh scripts/run.sh ${RUNTAG} ${SKIMVERSION} ${BABYVERSION} ${YEAR} ${USERNAME}
+    rm -rf /nfs-7/userdata/${USERNAME}/VBSHWWResult/${SKIMVERSION}/${BABYVERSION}/${YEAR}/${RUNTAG}
+    sh scripts/publish_result.sh ${RUNTAG} ${SKIMVERSION} ${BABYVERSION} ${YEAR}
+    if [[ "$BABYVERSION" == "v2" ]]; then
+        # Run everything else
+        python scripts/alpha_exp_study.py ${RUNTAG} ${SKIMVERSION} ${BABYVERSION} ${YEAR} ${USERNAME}
+        sh scripts/make_figures.sh ${RUNTAG} ${SKIMVERSION} ${BABYVERSION} ${YEAR} ${USERNAME}
+        ln -fs /nfs-7/userdata/${USERNAME}/VBSHWWResult/${SKIMVERSION}/${BABYVERSION}/${YEAR}/${RUNTAG}/plots plots
+        ln -fs /nfs-7/userdata/${USERNAME}/VBSHWWResult/${SKIMVERSION}/${BABYVERSION}/${YEAR}/${RUNTAG}/topbkgstudy topbkgstudy
+    fi
+done
+
