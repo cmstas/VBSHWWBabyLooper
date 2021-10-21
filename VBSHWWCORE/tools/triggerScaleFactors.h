86474b62462138f2f765bb30a2fdb66ff77d1a42
Jonathan Guiang <jkguiang@gmail.com>
2021-10-21 14:54:31 -0700

    fixed

--- VBSHWWCORE/tools/triggerScaleFactors.h
+++ VBSHWWCORE/tools/triggerScaleFactors.h
@@ -1,5 +1,6 @@
 #ifndef TRIGGERSCALEFACTORS_H
 #define TRIGGERSCALEFACTORS_H
+#include <math.h>
 
 float get2MuonTriggerEffSF(float pt, int year);
 float get2MuonTriggerEffSFErr(int year);
@@ -7,9 +8,13 @@ float getMuonElecTriggerEffSF(float pt, int year);
 float getMuonElecTriggerEffSFErr(int year);
 float get2ElecTriggerEffSF(float pt, int year);
 float get2ElecTriggerEffSFErr(int year);
+
 float getTauChannelElecTriggerSF(float eta, float pt, int year);
+
 float getTauChannelElecTriggerSFErr();
+
 float getTauChannelMuonTriggerSF(float eta, float pt, int year);
+
 float getTauChannelMuonTriggerSFErr();
 
 #endif
