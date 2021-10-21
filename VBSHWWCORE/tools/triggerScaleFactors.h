c4fe42ffc06423174b2cf07adcfa3e76d1be3ea6
Jonathan Guiang <jkguiang@gmail.com>
2021-10-21 14:58:11 -0700

    fixed

--- VBSHWWCORE/tools/triggerScaleFactors.h
+++ VBSHWWCORE/tools/triggerScaleFactors.h
@@ -8,13 +8,9 @@ float getMuonElecTriggerEffSF(float pt, int year);
 float getMuonElecTriggerEffSFErr(int year);
 float get2ElecTriggerEffSF(float pt, int year);
 float get2ElecTriggerEffSFErr(int year);
-
 float getTauChannelElecTriggerSF(float eta, float pt, int year);
-
 float getTauChannelElecTriggerSFErr();
-
 float getTauChannelMuonTriggerSF(float eta, float pt, int year);
-
 float getTauChannelMuonTriggerSFErr();
 
 #endif
