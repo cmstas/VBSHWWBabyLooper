9324d05dc03cfb3385c8c3328394f561f39669b1
Jonathan Guiang <jkguiang@gmail.com>
2021-10-21 15:12:56 -0700

    added ReReco pileup rwgts for kicks; added necessary STL imports

--- VBSHWWCORE/tools/pileUpReweight.h
+++ VBSHWWCORE/tools/pileUpReweight.h
@@ -1,5 +1,11 @@
 #ifndef PILEUPREWEIGHT_H
 #define PILEUPREWEIGHT_H
+#include <iostream>
+#include <stdlib.h>
+
+float pileUpReweight(int n_vtx, int year);
+float pileUpReweightUp(int n_vtx, int year);
+float pileUpReweightDown(int n_vtx, int year);
 
 float pileUpReweightUL(int n_vtx, int year);
 float pileUpReweightUpUL(int n_vtx, int year);
